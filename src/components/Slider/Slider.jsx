6cb33be0109d4394cbbe53b2c89c4b709d0dd3f7
daoxuanphuong1208 <daoxuanphuong1208@gmail.com>
2025-03-18 19:51:27 +0700

    Create and style slider component

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,88 @@
+import { Image } from "antd";
+import Slider from "react-slick";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import classNames from "classnames/bind";
+import styles from "./Slider.module.scss";
+
+const cx = classNames.bind(styles);
+
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "36px",
+        height: "36px",
+        right: "10px",
+        top: "50%",
+        borderRadius: "5px",
+        zIndex: "10",
+        background: "#ff901c",
+      }}
+      onClick={onClick}
+    >
+      <RightOutlined />
+    </div>
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "36px",
+        height: "36px",
+        left: "10px",
+        top: "50%",
+        borderRadius: "5px",
+        zIndex: "10",
+        background: "#ff901c",
+        color: "#fff !important",
+      }}
+      onClick={onClick}
+    >
+      <LeftOutlined />
+    </div>
+  );
+}
+
+const SliderComponent = (props) => {
+  const { arrImages } = props;
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+  return (
+    <Slider className={cx("wrapper")} {...settings}>
+      {arrImages.map((image, index) => {
+        return (
+          <Image
+            preview={false}
+            width="100%"
+            height="500px"
+            src={image}
+            key={index}
+            alt={`slider ${index}`}
+          />
+        );
+      })}
+    </Slider>
+  );
+};
+
+export { SliderComponent };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,18 @@
+import classNames from "classnames/bind";
+import styles from "./HomePage.module.scss";
+import { SliderComponent as Slider } from "../../components/Slider/Slider";
+
+import slider_1 from "../../assets/images/slider_1.webp";
+import slider_2 from "../../assets/images/slider_2.webp";
+import slider_3 from "../../assets/images/slider_3.webp";
+
+const cx = classNames.bind(styles);
 const HomePage = () => {
-  return <main>home</main>;
+  return (
+    <main className={cx("wrapper", "container")}>
+      <Slider arrImages={[slider_1, slider_2, slider_3]} />
+    </main>
+  );
 };
 
 export default HomePage;
