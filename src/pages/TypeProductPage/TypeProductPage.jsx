083b3392ae4ab5b4f6549c55dcf984cf2873bbd2
daoxuanphuong1208 <daoxuanphuong1208@gmail.com>
2025-03-20 21:35:56 +0700

    Change name ProductPage to TypeProductPage

--- src/pages/TypeProductPage/TypeProductPage.jsx
+++ src/pages/TypeProductPage/TypeProductPage.jsx
@@ -1,13 +1,17 @@
 import classNames from "classnames/bind";
-import styles from "./ProductPage.module.scss";
+import styles from "./TypeProductPage.module.scss";
+import { Pagination } from "antd";
 
 import ProductList from "../../components/ProductList/ProductList";
 import BoxWrapper from "../../components/BoxWrapper/BoxWrapper";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 
 const cx = classNames.bind(styles);
+const onChange = (pageNumber) => {
+  console.log("Page: ", pageNumber);
+};
 
-const ProductPage = () => {
+const TypeProductPage = () => {
   return (
     <div className={cx("wrapper", "container")}>
       <Breadcrumb />
@@ -21,8 +25,19 @@ const ProductPage = () => {
           <ProductList col={4} gap={10} />
         </div>
       </div>
+      <Pagination
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+        showQuickJumper
+        defaultCurrent={2}
+        total={500}
+        onChange={onChange}
+      />
     </div>
   );
 };
 
-export default ProductPage;
+export default TypeProductPage;
